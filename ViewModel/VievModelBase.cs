0e7bb168a760c9ba43d08dce496b3e9692af082f
Lorrenzino <vovan0vovan@gmail.com>
2023-10-05 14:32:16 +0300

    Класс основа моделей представления и реализация  INotifyPropertyChanged

---
+++ ViewModel/VievModelBase.cs
@@ -0,0 +1,35 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Statement_Sender_Client.ViewModel
+{
+    internal class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+
+        /// <summary>
+        /// переход на другую страницу +предварительная проверка на переход на туже самую страницу
+        /// </summary>
+
+        /*
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(PropertyName);
+            return true;
+        }
+        */
+    }
+}
